2d32f43a63fcc388293060f0fe95d9810214f7db
AlekseiSandakov <68049737+AlekseiSandakov@users.noreply.github.com>
2024-03-25 13:51:44 +0700

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -6,5 +6,5 @@ def test_different_languages(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     browser.get(link)
     time.sleep(10)
-    assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket"), \
+    assert browser.find_elements(By.CSS_SELECTOR, "button.btn-add-to-basket"), \
         'Кнопка добавить в корзину отсутсвует!'
